ef3e7a641fab0521560517cb41ef55b5b5942112
fengqz <18859287026@163.com>
2017-07-03 21:55:18 +0800

    add
    Signed-off-by: fengqz <18859287026@163.com>

--- src/main.js
+++ src/main.js
@@ -5,14 +5,10 @@ import MuseUI from 'muse-ui';
 import 'muse-ui/dist/muse-ui.css';
 import Element from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
-import VueResource from 'vue-resource';
 import store from './store/index';
-import InfiniteLoading from 'vue-infinite-loading';
-Vue.use(VueResource);
 Vue.use(MuseUI);
 Vue.use(Element);
 Vue.config.productionTip = false;
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
